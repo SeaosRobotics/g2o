ada6ef72096197f779c7dceef67c6f57c5a69dee
kylxyz <s-jin@seaos.co.jp>
2019-04-02 15:26:27 +0900

    return origin g2o

--- g2o/types/sba/types_sba.cpp
+++ g2o/types/sba/types_sba.cpp
@@ -267,15 +267,9 @@ namespace g2o {
     number_t py = pc(1);
     number_t pz = pc(2);
     number_t ipz2 = 1.0/(pz*pz);
-    //std::cout << "pz = " << pz << ", ipz2 = " << ipz2 << std::endl;
-    
     if (g2o_isnan(ipz2) ) {
       std::cout << "[SetJac] infinite jac" << std::endl;
-      //std::cout << "pz = " << pz << std::endl <<
-      std::cout <<"\nEdgeProjectP2SC::linearizeOplus()---------------------------------------\n";
-      pz = 3.8814e+12;
-      ipz2 = 6.63779e-26;
-      //abort();
+      abort();
     }
 
     number_t ipz2fx = ipz2*cam.Kcam(0,0); // Fx
@@ -360,11 +354,7 @@ namespace g2o {
     number_t ipz2 = 1.0/(pz*pz);
     if (g2o_isnan(ipz2) ) {
       std::cout << "[SetJac] infinite jac" << std::endl;
-      //std::cout << "pz = " << pz << std::endl <<
-      std::cout <<"\nEdgeProjectP2MC::linearizeOplus()---------------------------------------\n";
-      pz = 3.8814e+12;
-      ipz2 = 6.63779e-26;
-      //abort();
+      abort();
     }
 
     number_t ipz2fx = ipz2*cam.Kcam(0,0); // Fx
@@ -454,11 +444,7 @@ namespace g2o {
     number_t ipz2 = 1.0/(pz*pz);
     if (g2o_isnan(ipz2) ) {
       std::cout << "[SetJac] infinite jac" << std::endl;
-      //std::cout << "pz = " << pz << std::endl <<
-      std::cout <<"\nEdgeProjectP2MC_Intrinsics::linearizeOplus()---------------------------------------\n";
-      pz = 3.8814e+12;
-      ipz2 = 6.63779e-26;
-      //abort();
+      abort();
     }
 
     number_t ipz2fx = ipz2*cam.Kcam(0,0); // Fx
